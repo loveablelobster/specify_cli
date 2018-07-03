78d23d9768262e9abf5816fa6a1540c2480cc54b
martin <loveablelobster@fastmail.fm>
2018-07-03 11:37:34 +0200

    specs for CE embedding

--- spec/models/collection_object_spce.rb
+++ spec/models/collection_object_spce.rb
@@ -8,8 +8,31 @@ module Specify
       end
 
       it 'bla' do
-        p collection
         p collection.add_collection_object CatalogNumber: '1'
+      end
+
+      context 'when adding to collection' do
+      	context 'when collecting event is emebedded' do
+      		subject do
+      		  collection.add_collection_object(CatalogNumber: '1')
+      		end
+
+      		it do
+      		  ce = an_instance_of(Specify::Model::CollectingEvent)
+      			is_expected.to have_attributes collecting_event: ce
+      		end
+      	end
+
+      	context 'when collecting event is not embedded' do
+          subject do
+          	Collection.first(Code: 'NECE')
+          	          .add_collection_object(CatalogNumber: '2')
+          end
+
+          it do
+            is_expected.to have_attributes collecting_event: nil
+          end
+      	end
       end
     end
   end
