9bb92298a39985e4163d586d92c88c5ba3ff7444
Martin Stein <loveablelobster@fastmail.fm>
2018-08-14 16:09:39 +0200

    documentation

--- lib/specify/version.rb
+++ lib/specify/version.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 module Specify
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
   SUMMARY = 'A command line interface for Specify'
   DESCRIPTION = <<~HEREDOC
-    specify_cli allows to carry out certain tasks on a Specify database
-    (http://www.sustain.specifysoftware.org) from the command line.
+    specify_cli is a tool that allows certain tasks in a Specify database
+    (http://www.sustain.specifysoftware.org) to be carried out from the command
+    line.
     Currently supported tasks:
     - upload of views to the database
     - generation of stub records
